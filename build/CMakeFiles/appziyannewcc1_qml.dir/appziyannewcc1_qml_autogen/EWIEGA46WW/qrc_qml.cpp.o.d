2258788aa63cff7d392182bfb8b1c7c5f13311e3
LethargyTeddy <52397629+LethargyTeddy@users.noreply.github.com>
2025-08-04 10:43:19 +0800

    Revert "delete新建文件"

--- build/CMakeFiles/appziyannewcc1_qml.dir/appziyannewcc1_qml_autogen/EWIEGA46WW/qrc_qml.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/appziyannewcc1_qml.dir/appziyannewcc1_qml_autogen/EWIEGA46WW/qrc_qml.cpp.o: \
-  /root/Desktop/boatapp/build/appziyannewcc1_qml_autogen/EWIEGA46WW/qrc_qml.cpp
---
+++ build/Desktop_Qt_6_9_1_MSVC2022_64bit-Debug/.qt/bin/qt_setup_tool_path.bat
@@ -0,0 +1,3 @@
+@echo off
+set PATH=D:\Qt\QT6\6.9.1\msvc2022_64\bin;%PATH%
+%*
